4e4efa7c58e3e7afe1363756c7fdc1c442c5ab89
Cheryl-Chun(Mac) <769303522@qq.com>
2024-05-13 15:24:54 +0800

    Site updated: 2024-05-13 15:24:53

---
+++ scripts/main.js
@@ -0,0 +1,169 @@
+// 代码复制
+const copyToClipboard = (text) => {
+    // console.log(text)
+    navigator.clipboard.writeText(text)
+        .then(() => {
+            console.log('文本已成功复制到剪贴板');
+        })
+        .catch(err => {
+            console.error('复制失败：', err);
+        });
+}
+
+(function() {
+    'use strict';
+    const asciiBanner = ` 
+     ____  _                         _ <br>
+    / ___|| |__    ___  _ __  _   _ | |<br>
+   | |    | '_ \\  / _ \\| '__|| | | || |<br>
+   | |___ | | | ||  __/| |   | |_| || |<br>
+    \\____||_| |_| \\___||_|    \\__, ||_|<br>
+                              |___/    
+  `;
+    console.log("js loaded successfully")
+    
+    // 滚动条
+    $(".main-content").mCustomScrollbar({
+        theme: "rounded-dots",
+        mouseWheel:{scrollAmount:400},
+        scrollButtons:{
+            enable:true,
+            scrollType:"continuous",
+            scrollSpeed:40,
+            scrollAmount:40,
+            style:"crosshair"
+        },
+    });
+
+    $('.btn-close').hover(function(e) {
+        let btnClose = $('.btn-close')[0];
+        btnClose.src = '/images/titlebutton-close-hover-alt.png';
+    });
+    let topbarBtn = $('.btn-container')[0];
+    let close = $('.btn-close')[0];
+    let minimize = $('.btn-minimize')[0];
+    let maximize = $('.btn-maximize')[0];
+    topbarBtn.addEventListener('mouseover', function(e) {
+        close.src = '/images/close-hover.svg';
+        minimize.src = '/images/minimize-hover.svg';
+        maximize.src = '/images/maximize-hover.svg';
+    });
+    topbarBtn.addEventListener('mouseout', function(e) {
+        close.src = '/images/close.svg';
+        minimize.src = '/images/minimize.svg';
+        maximize.src = '/images/maximize.svg';
+    });
+
+    const banner = $('.banner-container')[0]
+    if (banner != undefined) {
+        banner.innerHTML = asciiBanner;
+    }
+    
+    // 首页动作监听
+    const class_name = [".category", ".tag", ".archive", ".other"];
+    for (let name of class_name) {
+        const btns = $(`${name}-show-btn`);
+        for (let i = 0; i < btns.length; i++) {
+            const btn = btns[i];
+            if (btn != undefined) {
+                btn.addEventListener('click', function(e) {
+                    const item = $(`${name}`)[0];
+                    if (item != undefined) {
+                        item.classList.toggle('collapsed');
+                    }
+                });
+                btn.addEventListener('mouseover', function(e) {
+                    btn.children[0].classList.remove('is-hover');
+                });
+                btn.addEventListener('mouseout', function(e) {
+                    btn.children[0].classList.add('is-hover');
+                });
+            }
+        }
+    }
+
+    // 打字机 
+      
+    const writer = $('.type-writer')[0];
+    if (writer != undefined) {
+        const text = writer.textContent.trim().split('\n');
+        writer.textContent = "";
+        const options = {
+            strings: text,
+            typeSpeed: 100,
+            backSpeed: 70,
+            loop: true,
+        }
+        new Typed(writer, options);
+    }
+
+    if ($(".github-calendar")[0] != undefined) {
+        GitHubCalendar(".github-calendar", "qiancijun");
+    }
+
+    // 代码复制  
+    const code_blocks = $('.highlight');
+    // console.log(code_blocks);
+    for (let i = 0; i < code_blocks.length; i++) {
+        const block = code_blocks[i];
+        $(block).attr("id", `code-${i}`)
+        const copy_btn = `
+        <div class="copy" id="copy-btn-${i}" data-clipboard-target="#code-${i}" style="width:16px; height: 16px; position: absolute; right: 10px; top: 10px; cursor: pointer"><img src="/images/copy.svg"></div>
+        `
+        
+        $(block).prepend(copy_btn);
+        let clipboard = new ClipboardJS(`#copy-btn-${i}`);
+        clipboard.on('success', function(e) {
+            Toastify({
+                text: "Copy success",
+                duration: 3000,
+                // destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                  background: "rgba(45, 45, 45, 0.5)",
+                  color: "color-text"
+                },
+                onClick: function(){} // Callback after click
+              }).showToast();
+            e.clearSelection();
+          });
+        
+        clipboard.on('error', function(e) {
+            Toastify({
+                text: "Copy failed",
+                duration: 3000,
+                // destination: "https://github.com/apvarun/toastify-js",
+                newWindow: true,
+                close: true,
+                gravity: "top", // `top` or `bottom`
+                position: "right", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                  background: "rgba(45, 45, 45, 0.5)",
+                  color: "color-text"
+                },
+                onClick: function(){} // Callback after click
+              }).showToast();
+        });
+    }
+
+    // 引用展开
+    const quotes = $('blockquote');
+    for (let i = 0; i < quotes.length; i++) {
+        const quote = quotes[i];
+        const expand_btn = `
+        <div id="expand-btn-${i}">
+            <img style="position: absolute; top:0em; right: 1em; cursor: pointer;" src="/images/expand.svg">
+        </div>
+        `
+        $(quote).prepend(expand_btn)
+        $(quote).attr("id", `quote-${i}`)
+        $(`#expand-btn-${i}`)[0].addEventListener('click', function() {
+            $(`#quote-${i}`)[0].classList.toggle('expanded'); 
+        });
+    }
+})();
---
+++ scripts/search.js
@@ -0,0 +1,141 @@
+const wrap_html = (data, author) =>  {
+    const data_url = data.url;
+    const data_tags = data.tags;
+    const data_categories = data.categories;
+    const data_date = moment(data.date).format('MMMM Do YYYY, h:mm:ss a');
+    const data_tag_number = data.tag_number;
+    const data_title = data.title;
+    let str = "";
+    str += '<tr class="post-list-item">';
+    str += '<td><span class="article-authority"># -rw-r--r--</span></td>';
+    str += `<td><span class="link">${data_tag_number}</span></td>`
+    str += `<td><span class="author">${author}</span></
+    td>`
+    str += `<td><span class="author">${author}</span></
+    td>`
+    str += '<td><span class="group">root</span></td>';
+    str += `<td>${data_date}</td>`
+    
+    str += "<td>"
+    for (let i = 0; i < data.categories.length; i++) {
+        const category = data.categories[i];
+        if (category != "") {
+            str += `<a class="category-link" href="/categories/${category}/" rel="category">${i != data_categories.length - 1 ? category + "," : category}</a>`;
+        }
+    }
+    str += "</td>"
+
+    str += "<td>"
+    for (let i = 0; i < data_tags.length; i++) {
+        const tag = data_tags[i];
+        if (tag != "") {
+            str += `<a class="tag-link" href="/tags/${tag}/" rel="tag">${i != data_tags.length - 1 ? tag + "," : tag}</a>`;
+        }
+    }
+    str += "</td>"
+
+    str += `<td><a href="${data_url}" class="title">${data_title}</a></td>`
+    str += "</tr>";
+    return str;
+}
+
+const init = (datas, author) => {
+    let str = '<table class="post-list">';
+    datas.forEach(data => {
+        str += wrap_html(data, author)
+    })
+    str += '</table>'
+    return str
+}
+
+var searchFunc = function(path, search_id, content_id, author) {
+    const $resultContent = document.getElementById(content_id);
+    const $ql = document.getElementById("query-language");
+            
+    if ($resultContent == undefined) {
+        return;
+    }
+    'use strict';
+    $.ajax({
+        url: path,
+        dataType: "xml",
+        success: function( xmlResponse ) {
+            // get the contents from search data
+            const datas = $( "entry", xmlResponse ).map(function() {
+                return {
+                    title: $( "title", this ).text().trim(),
+                    url: $( "url" , this).text().trim(),
+                    categories: $( "categories", this).text().trim().split("\n").map(c => c.trim()),
+                    tags: $("tags", this).text().trim().split("\n").map(tag => tag.trim()),
+                    date: $("date", this).text().trim(),
+                    tag_number: $("tag-number", this).text()
+                };
+            }).get();
+
+            // 第一次加载全数据
+            const html = init(datas, author)
+            $resultContent.innerHTML = html;
+
+            const $input = document.getElementById(search_id);
+			if (!$input) return;
+            
+            if ($("#local-search-input").length > 0) {
+                $input.addEventListener('input', function () {
+                    let str = '<table class="post-list">';
+                    let keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+                    
+                    if ($ql != undefined) {
+                        $ql.textContent = `WHERE title LIKE %${keywords}% OR tags in (${keywords}) OR category in (${keywords})`
+                    }
+
+                    $resultContent.innerHTML = "";
+                    if (this.value.trim().length <= 0) {
+                        $resultContent.innerHTML = html;
+                        $ql.textContent = "";
+                        return;
+                    }
+
+                    datas.forEach(function (data) {
+                        let isMatch = true;
+                        if (!data.title || data.title.trim() === '') {
+                            data.title = "Untitled";
+                        }
+                        const data_title = data.title.trim().toLowerCase();
+                        const data_tags = data.tags;
+                        const data_categories = data.categories;
+                        let index_title = -1;
+                        let tag_catch = false;
+                        let category_catch = false;
+                        // hello world
+                        keywords.forEach(function (keyword, i) {
+                            index_title = data_title.indexOf(keyword);
+                            if (index_title < 0) {
+                                isMatch = false;
+                            }
+                            for (let tag of data_tags) {
+                                tag = tag.toLowerCase();
+                                if (tag.includes(keyword)) {
+                                    tag_catch = true;
+                                    break;
+                                }
+                            }
+                            for (let category of data_categories) {
+                                category = category.toLowerCase();
+                                if (category.includes(keyword)) {
+                                    category_catch = true;
+                                    break;
+                                }
+                            }
+                        });
+                        // show search results
+                        if (isMatch || tag_catch || category_catch) {
+                            str += wrap_html(data, author)
+                        }
+                    });
+                    str += "</table>";
+                    $resultContent.innerHTML = str;
+                });
+            }
+        }
+    });
+}
